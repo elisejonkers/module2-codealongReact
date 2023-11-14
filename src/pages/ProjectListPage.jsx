22802a04cbb75df0cf2ee62e63be5034414ea792
“elisejonkers” <elisejonkers18@hotmail.com>
2023-11-14 11:59:24 +0100

    add useEffect

--- src/pages/ProjectListPage.jsx
+++ src/pages/ProjectListPage.jsx
@@ -1,39 +1,44 @@
-import axios from "axios"
-import { useEffect, useState } from "react"
-import { Link } from "react-router-dom"
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
-//const API_URL = "https://project-management-api-4641927fee65.herokuapp.com/"
+const API_URL = "https://project-management-api-4641927fee65.herokuapp.com/"
 
+function ProjectListPage() {
+  console.log("ProjectListPage has been invoked...");
 
-function ProjectListPage (){
+  const [projects, setProjects] = useState([]);
 
-    const [projects, setProjects] = useState([])
+  const getAllProjects = () => {
+    axios
+      .get(API_URL + "projects?_embed=tasks")
+      .then((response) => {
+        setProjects(response.data);
+      })
+      .catch((error) => {
+        console.log("Error getting from API: " + error);
+      });
+  };
 
-    useEffect(function (){
-        axios.get("https://project-management-api-4641927fee65.herokuapp.com/projects?_embed=tasks")
-        .then( (response) => {
-            setProjects(response.data)
-        })
-        .catch( (error) => {
-            console.log("Error getting from API: " + error)
-        })
-    }, [])
+  useEffect(() => {
+    getAllProjects();
+  }, []);
 
-    return (
-      <div className="ProjectListPage">
-        <h1>List of projects:</h1>
+  return (
+    <div className="ProjectListPage">
+      <h1>List of projects:</h1>
 
-        {projects.map((project) => {
-          return (
-            <div className="ProjectCard card" key={project.id}>
-              <Link to={`/projects/${project.id}`}>
-                <h3>{project.title}</h3>
-              </Link>
-            </div>
-          );
-        })}
-      </div>
-    );
+      {projects.map((project) => {
+        return (
+          <div className="ProjectCard card" key={project.id}>
+            <Link to={`/projects/${project.id}`}>
+              <h3>{project.title}</h3>
+            </Link>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
-export default ProjectListPage
+export default ProjectListPage;
